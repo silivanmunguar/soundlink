ba08443fc3bf4ea4d726e13cc6f8c103aed5e0c1
Silivan Munguarakarama <silivan99@gmail.com>
2023-06-03 01:22:54 +0000

    forgot to add the files i made changes to in the last two commit messages

---
+++ resources/General/__tests__/trackParamsExtractor.test.js
@@ -0,0 +1,94 @@
+import TrackParamsExtractor from '../TrackParamsExtractor'
+import { describe, it, expect } from '@jest/globals' // for the linter to stop complaining
+
+// SPOTIFY TESTS
+describe('Testing TrackParamsExtractor to extract serviceProvider and trackId from a shared url', () => {
+  describe('SPOTIFY', () => {
+    it('should extract the track parameters from the shared URL', () => {
+      // Arrange
+      const trackParamsExtractor = new TrackParamsExtractor()
+      trackParamsExtractor.sharedUrl =
+        'https://open.spotify.com/track/7BH6nyhnWTSfMUwwrCYbJF?si=44f5565533da4d74'
+
+      // Act
+      const trackParams = trackParamsExtractor.extractTrackParams()
+
+      // Assert
+      expect(trackParams).toEqual({
+        serviceProvider: 'spotify',
+        trackId: '7BH6nyhnWTSfMUwwrCYbJF'
+      })
+    })
+
+    it('should return null if the shared URL is not set', () => {
+      // Arrange
+      const trackParamsExtractor = new TrackParamsExtractor()
+
+      // Act
+      const trackParams = trackParamsExtractor.extractTrackParams()
+
+      // Assert
+      expect(trackParams).toBeNull()
+    })
+  })
+
+  // DEEZER TESTS
+  describe('DEEZER', () => {
+    it('should extract the track parameters from the shared URL', () => {
+      // Arrange
+      const trackParamsExtractor = new TrackParamsExtractor()
+      trackParamsExtractor.sharedUrl =
+        'https://www.deezer.com/track/3135556?utm_source=deezer&utm_content=track-3135556&utm_term=0_1590699905&utm_medium=web'
+
+      // Act
+      const trackParams = trackParamsExtractor.extractTrackParams()
+
+      // Assert
+      expect(trackParams).toEqual({
+        serviceProvider: 'deezer',
+        trackId: '3135556'
+      })
+    })
+
+    it('should return null if the shared URL is not set', () => {
+      // Arrange
+      const trackParamsExtractor = new TrackParamsExtractor()
+
+      // Act
+      const trackParams = trackParamsExtractor.extractTrackParams()
+
+      // Assert
+      expect(trackParams).toBeNull()
+    })
+  })
+
+  // APPLE MUSIC TESTS
+  describe('APPLE MUSIC', () => {
+    it('For Apple Music: should extract the track parameters from the shared URL', () => {
+      // Arrange
+      const trackParamsExtractor = new TrackParamsExtractor()
+      trackParamsExtractor.sharedUrl =
+        'https://music.apple.com/us/album/la-gota-fr%C3%ADa/1543991915?i=1543991935'
+
+      // Act
+      const trackParams = trackParamsExtractor.extractTrackParams()
+
+      // Assert
+      expect(trackParams).toEqual({
+        serviceProvider: 'apple',
+        trackId: '1543991915'
+      })
+    })
+
+    it('For Apple Music: should return null if the shared URL is not set', () => {
+      // Arrange
+      const trackParamsExtractor = new TrackParamsExtractor()
+
+      // Act
+      const trackParams = trackParamsExtractor.extractTrackParams()
+
+      // Assert
+      expect(trackParams).toBeNull()
+    })
+  })
+})
---
+++ resources/Services/Deezer/TrackDetailsExtraction/DeezerTrackDetailsExtraction.js
@@ -0,0 +1,48 @@
+import TrackDetailsExtraction from '../../../Interfaces/TrackDetailsExtraction.js'
+import DeezerTrackDetailsExtractionHelper from './DeezerTrackDetailsExtractionHelper.js'
+
+class DeezerTrackDetailsExtraction extends TrackDetailsExtraction {
+  async getTrackDetails (trackId) {
+    /* TODO: Find a direct way to get track details from Deezer API
+    currently you are quering the share url and then extracting the track
+    url that contains the id and then using the id to quesry track details using a helper class
+    */
+    const helper = new DeezerTrackDetailsExtractionHelper()
+    this.trackId = await helper.getTrackId(trackId)
+
+    // Set the url
+    const url = `${process.env.DEEZER_API_TRACK_QUERY_URL}${this.trackId}`
+
+    // Set the timeout
+    const TIMEOUT = 5000
+
+    // Get the response
+    const response = await fetch(url, {
+      timeout: TIMEOUT
+    })
+
+    // Check if the response is valid
+    if (response.status !== 200) {
+      const message = await response.json()
+      throw new Error(message.error.message)
+    }
+
+    // Get the track details
+    const responseJSON = await response.json()
+
+    // Set the track details
+    this.trackDetails = {
+      title: responseJSON.title,
+      artist: responseJSON.artist.name,
+      album: responseJSON.album.title,
+      externalUrl: responseJSON.link
+    }
+
+    // Return the track details
+    // TODO:check forr different titles versions or son versions. ie. David Bisbal {Apiádate de Mi (Grabación en Estudio)} vs David Bisbal {Apiádate de Mi}
+    // Do this for all services
+    return this.trackDetails
+  }
+}
+
+export default DeezerTrackDetailsExtraction
