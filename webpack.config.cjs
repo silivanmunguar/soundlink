c81c7112f530319b09906d7a13b27e1534612b15
Silivan Munguarakarama <silivan99@gmail.com>
2023-06-25 20:26:29 -0500

    attempting to build app

---
+++ webpack.config.cjs
@@ -0,0 +1,9 @@
+// const path = require('path')
+
+module.exports = {
+  entry: './webapp/src/index.js',
+  output: {
+    filename: 'main.js',
+    path: '/home/buzzbeebaby/SoundLink/webapp/dist'
+  }
+}
