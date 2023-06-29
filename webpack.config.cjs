d48960594d6d80f349d474aef53a06e62a30abc8
Silivan Munguarakarama <silivan99@gmail.com>
2023-06-29 17:41:55 -0500

    prep wok for react app

--- webpack.config.cjs
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  entry: './webapp/src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'webapp/dist')
-  }
-}
