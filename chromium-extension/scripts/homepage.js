a5a538c829ab30861549f75dfd9b77355f827dec
Silivan Munguarakarama <silivan99@gmail.com>
2023-07-10 18:40:56 -0500

    added default checkmark logic

--- chromium-extension/scripts/homepage.js
+++ chromium-extension/scripts/homepage.js
@@ -1,15 +1,77 @@
+let defaultService;
+
+// get the user default setting from storage and mark the button as active
+getDefaultService();
+
 //get the user default setting button
-function handleClick(id) {
-  defaultService = id;
-  console.log("default service set to: ", defaultService);
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
+
+// get user defualt setting from storage and add the active class to the button
+function getDefaultService() {
+  chrome.storage.sync.get("userSettings", (data) => {
+    const userSettings = data.userSettings;
+    if (userSettings.defaultService) {
+      // add the active class to the button
+      buttons.forEach((button) => {
+        if (button.dataset.service === userSettings.defaultService) {
+          button.classList.add("active");
+          // create a checkmark
+          createCheckmark(button);
+        }
+      });
+    }
+  });
+  return null;
 }
 
-// save user default setting to storage
-const userSettings = {
-  defaultService: defaultService,
-  blockedServices: [1],
-};
+// get user default service from
+function setDefaultService(defaultService) {
+  // save user default setting to storage
+  const userSettings = {
+    defaultService: defaultService,
+    blockedServices: [1],
+  };
 
-chrome.storage.sync.set({ userSettings: userSettings }, () => {
-  console.log("User settings saved to storage:", userSettings);
-});
+  chrome.storage.sync.set({ userSettings: userSettings }, () => {
+    console.log("User settings saved to storage:", userSettings.defaultService);
+  });
+}
+
+// handle the click event
+function handleClick() {
+  // remove the active class from all buttons
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+    // remove the checkmark if it exists
+    if (button.querySelector(".checkmark")) {
+      button.querySelector(".checkmark").remove();
+    }
+  });
+  // get the clicked button
+  const activebutton = this;
+  // set the clicked button to active
+  activebutton.classList.add("active");
+  // create a checkmark
+  createCheckmark(activebutton);
+  // set the user default setting
+  defaultService = this.dataset.service;
+  // set the default service
+  setDefaultService(defaultService);
+}
+
+function createCheckmark(activebutton) {
+  // create a checkmark to show the user which button is active
+  const checkmark = document.createElement("span");
+
+  // add the checkmark class to the checkmark
+  checkmark.classList.add("checkmark");
+
+  // add the checkmark image to the checkmark
+  checkmark.innerHTML = `<img src="../../assets/icons/icons8-checkmark-500.png" alt="checkmark" />`;
+
+  // add the checkmark to the clicked button
+  activebutton.appendChild(checkmark);
+}
--- chromium-extension/service_worker.js
+++ chromium-extension/service_worker.js
@@ -1,32 +1,79 @@
 "use strict";
 
+let defaultService;
+
+async function init() {
+  defaultService = await getDefaultService();
+}
+
 // get user defualt setting from storage
-chrome.storage.sync.get("userSettings", (data) => {
-  const userSettings = data.userSettings;
-  if (userSettings) {
-    // set the rules
-    chrome.declarativeNetRequest.updateDynamicRules({
-      removeRuleIds: userSettings.blockedServices,
+function getDefaultService() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get("userSettings", (data) => {
+      const userSettings = data.userSettings;
+      if (userSettings && userSettings.defaultService) {
+        console.log("User settings retrieved:", userSettings.defaultService);
+        resolve(userSettings.defaultService);
+      } else {
+        reject("No default service found");
+      }
     });
+  });
+}
+
+// udpate user settings in storage if the user changes the default service
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (changes.userSettings) {
+    const userSettings = changes.userSettings.newValue;
+    defaultService = userSettings.defaultService;
   }
 });
 
-// lsiten for rule matches from the background script
+// listen for rule matches from the background script
 chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(async (e) => {
   console.log("Rule matched: ", e.request.url);
 
-  const apiurl = `https://soundlink-api.onrender.com/api/track?url=${e.request.url}`;
-  try {
-    const data = await (await fetch(apiurl)).json();
+  // if the user has no default service do nothing
+  if (!defaultService) {
+    console.log("No default service found");
+    return;
+  }
 
-    // redirect to the track page
-    chrome.tabs.update(e.tabId, {
-      url: data.deezer.track.externalUrl,
-    });
-    // redirect to a new url
-  } catch (error) {
-    console.error(error);
+  // if the current link is not the default service
+  if (!e.request.url.includes(defaultService)) {
+    // get the track data from the api
+    // const apiurl = "https://soundlink-api.onrender.com/api/track?";
+    const apiurl = "http://localhost:3001/api/track?";
+
+    // request body
+    const body = {
+      url: e.request.url,
+      defaultService: defaultService,
+    };
+
+    // make a post request to the api to get the track data
+    try {
+      const data = await fetch(apiurl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }).then((res) => res.json());
+      // redirect to the track page
+      chrome.tabs.update(e.tabId, {
+        // dynamically access the service that corresponds to the defaultService
+        url: data[defaultService].track.externalUrl,
+      });
+      // redirect to a new url
+    } catch (error) {
+      console.log("error found:", error);
+    }
+  } else {
+    // if the current link is the default service do nothing
+    console.log("default ", defaultService, " exists! do nothing");
   }
 });
 
 console.log("Service Worker has Loaded...");
+init();
